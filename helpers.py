fd5695d872448fc33adf8b1c4720294b0534de9c
Karin Anderson <kanderson@CourtlandsMBP2.lan>
2020-06-01 13:16:26 +0100

    pushing to heroku

--- helpers.py
+++ helpers.py
@@ -14,6 +14,6 @@ def login_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
         if session.get("user_id") is None:
-            return redirect(url_for('login', next=request.endpoint, id=request.path[7:11]))
+            return redirect(url_for('login', next=request.endpoint, id=request.path[7:13]))
         return f(*args, **kwargs)
     return decorated_function
